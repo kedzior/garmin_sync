0876e5e264186c12f4ef3e597ace26846e97bede
Łukasz Kędziorski <kedziorski.lukasz@gmail.com>
2018-03-26 09:26:40 +0200

    Change double quotes to single quotes

--- device_scan.py
+++ device_scan.py
@@ -4,17 +4,17 @@ import os.path
 class DeviceScan:
     def scan(self):
         devices = []
-        with open("/proc/partitions", "r") as f:
+        with open('/proc/partitions', 'r') as f:
             for line in f.readlines()[2:]:
                 columns = [column.strip() for column in line.split()]
                 minor_number = int(columns[1])
                 name = columns[3]
 
                 if (minor_number % 16) == 0:
-                    path = "/sys/class/block/" + name
+                    path = '/sys/class/block/' + name
 
                     if os.path.islink(path):
-                        if os.path.realpath(path).find("/usb") > 0:
-                            devices.append("/dev/" + name)
+                        if os.path.realpath(path).find('/usb') > 0:
+                            devices.append('/dev/' + name)
 
         return devices
